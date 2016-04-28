8cbae481151a46ec521f135fac78b60577b06236
julien-colot <julien.colot@gmail.com>
2016-04-28 23:31:56 +0200

    Update strategy.h

--- strategy.h
+++ strategy.h
@@ -1,6 +1,6 @@
 #ifndef STRATEGY_H
 #define STRATEGY_H
-int getBestWorstScore(game_t * game, int move, int depth, int maxDepth, int alpha, int beta);
+int getBestValue(game_t * game, int move, int depth, int maxDepth, int alpha, int beta);
 int getBestMove(game_t * game, int maxDepth); 
 int evalBoard(game_t * game);
 #endif
