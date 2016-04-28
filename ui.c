8fa8dd70e8e52eae596d44b2bf80633098c84ac0
julien-colot <julien.colot@gmail.com>
2016-04-28 17:11:27 +0200

    Update ui.c

--- ui.c
+++ ui.c
@@ -191,6 +191,23 @@ void stateMachine() {
 
     char input[80];
     state_t state = INIT_STATE;
+    
+/* 
+ * Si firstPlayer =  0, c'est human qui joue en premier
+ * Si firstPlayer =  1, c'est computer qui joue en premier
+ * Si firstPlayer = -1, c'est initGame qui decide aleatoirement
+ */
+    
+    int firstPlayer = -1;
+
+/*
+ * Si iniSeeds = -1, le nombre initial de graines 
+ * est decide aleatoirement par initGame
+ */
+
+    int iniSeeds = -1;
+    int depth = DEPTH;
+    int tmp;
     game_t * game;
 
     printf("\n\n");
@@ -207,6 +224,7 @@ void stateMachine() {
                
                 printf("Make a selection:\n\n");
                 printf("(P)lay\n");
+                printf("(C)onfigure the game\n\n");
                 printf("(Q)uit the game\n\n");
                 printf("Your choice> ");
 
@@ -223,28 +241,174 @@ void stateMachine() {
                     case 'P':
                         state = PLAY_STATE;
                         break;
+                        
+                    case 'C':
+                        state = CONFIG_STATE;
+                        break;
 
                     default:
                         printf("\nError: entry '%c' is not valid\n\n", input[0]);
                 }
                 
                 break;
-
+                
             case PLAY_STATE:
 /*
  * On delegue la gestion du jeu a 
  * la fonction playGame, jusqu'a la fin de la partie
  */ 
-                game = initGame();
+                game = initGame(firstPlayer, iniSeeds);
                 displayBoard(game);
                 state = playGame(game);
                 break;
+                
+            case CONFIG_STATE:
+/*
+ * Configuration
+ * 
+ */ 
+                printf("Select the parameter to configure:\n\n");
+                printf("(F)irst player\n");
+                printf("(D)ifficulty\n");
+                printf("(I)nitial number of seeds per bowl\n");
+                printf("(P)lay\n\n");
+                printf("Your choice> ");
+               
+               if (fgets(input, 80, stdin) == NULL) {
+                    state = FAILURE_STATE;
+                }
 
+                switch(toupper(input[0])) {
+
+                    case 'F':
+                        state = FIRSTPLAYER_STATE;
+                        break;
+                    
+                    case 'I':
+                        state = INISEEDS_STATE;
+                        break;
+                        
+                    case 'D':
+                        state = DEPTH_STATE;
+                        break;
+                        
+                    case 'P':
+                        state = PLAY_STATE;
+                        break;
+
+                    default:
+                        printf("\nError: entry '%c' is not valid\n\n", input[0]);
+                }
+                
+                break;
+                
+            case FIRSTPLAYER_STATE:
+/*
+ * Configuration du joueur qui commence
+ * 
+ */ 
+                printf("Select who makes the first move:\n\n");
+                printf("(C)omputer\n");
+                printf("(Y)ou\n");
+                printf("(R)andom\n\n");
+                printf("Your choice> ");
+                
+                if (fgets(input, 80, stdin) == NULL) {
+                    state = FAILURE_STATE;
+                }
+
+                switch(toupper(input[0])) {
+                    
+                    case 'C':
+                        firstPlayer = 1;
+                        printf ("\nComputer will play first\n")};
+                        state = CONFIG_STATE;
+                        break;
+                        
+                    case 'Y':
+                        firstPlayer = 0;
+                        printf ("\nYou'll play first\n")};
+                        state = CONFIG_STATE;
+                        break;
+                        
+                    case 'R':
+                        firstPlayer = -1;
+                        printf ("\nFirst player set randomly at the beginning of the game\n\n")};
+                        break;
+                        
+                    case 'P':
+                        state = PLAY_STATE;
+                        break;
+                        
+                    case 'Q':
+                        state = EXIT_STATE;
+                        break;
+                        
+                    default:
+                        printf("\nError: entry '%c' is not valid\n\n", input[0]);
+                }
+                
+                break;
+                
+            case DEPTH_STATE:
+/*
+ * Configuration de la profondeur d'arbre a explorer
+ * 
+ */ 
+                printf ("Define the difficulty level (0 is easiest and 15 is hardest)\n\n");
+                printf("Your choice> ");
+               
+                if (fgets(input, sizeof(input), stdin) != NULL) {
+                   tmp = strtol(input, &p, 10);
+                   
+                    if (input[0] != '\n' && (*p == '\n' || *p == '\0')) {
+                        if (tmp >= 0 && tmp <= 15) {
+                            depth = tmp;
+                            printf ("\nDifficulty level set to \n\n", depth);
+                        }
+                    } else {
+                        printf("\nError: entry '%s' is not valid\n\n", input);
+                    }
+                } else {
+                    state = FAILURE_STATE;
+                }
+                
+                break;
+                
+            case INISEEDS_STATE:
+/*
+ * Configuration de la profondeur d'arbre a explorer
+ * 
+ */ 
+                printf ("Define the initial number of seeds (between 1 and 6, or 'R' for random)\n\n");
+                printf("Your choice> ");
+               
+                if (fgets(input, sizeof(input), stdin) != NULL) {
+                   tmp = strtol(input, &p, 10);
+                   
+                    if (input[0] != '\n' && (*p == '\n' || *p == '\0')) {
+                        if (tmp >= 1 && tmp <= 6) {
+                            depth = tmp;
+                            printf ("\n\nInitial number of seeds set to \n", depth);
+                            state = CONFIG_STATE;
+                        }
+                    } else if (toupper(input[0] = 'R') { 
+                        printf ("\n\nIntial number of seeds set randomly at the beginning of the game\n")};
+                        state = CONFIG_STATE;
+                    } else { 
+                        printf("\nError: entry '%s' is not valid\n\n", input);
+                    }
+                } else {
+                    state = FAILURE_STATE;
+                }
+                
+                break;
+                
             case FAILURE_STATE :
 /*
  * Juste au cas ou, en cas de probleme avec fgets
  */ 
-                fprintf(stderr, "There was an unknown technical issue...");
+                fprintf(stderr, "There was an unknown technical issue...\n\n");
                 state = EXIT_STATE;
                 break;
  
