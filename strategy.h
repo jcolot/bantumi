4c77dca30d2f57b78956af30401b6f9f5da46212
julien-colot <julien.colot@gmail.com>
2016-05-01 00:08:06 +0200

    Update strategy.h

--- strategy.h
+++ strategy.h
@@ -3,4 +3,5 @@
 int getBestValue(game_t * gameStack, int move, int depth, int maxDepth, int alpha, int beta, long long maxTime);
 int getBestMove(game_t * game, int maxDepth); 
 int evalBoard(game_t * game);
+long long currentTimestamp();
 #endif
